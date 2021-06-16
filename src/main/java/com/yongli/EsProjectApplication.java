41681986228a510169141717dcbd51378df4813e
liyong <1948505413@qq.com>
2021-06-16 18:53:07 +0800

    第一次提交

---
+++ src/main/java/com/yongli/EsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yongli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsProjectApplication.class, args);
+	}
+
+}
