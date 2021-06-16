b93d9128f3bfa872c310b0d8b7db5667fb964464
liyong <1948505413@qq.com>
2021-06-16 19:28:12 +0800

    es配置类加载

---
+++ src/main/java/com/yongli/config/EsConfig.java
@@ -0,0 +1,19 @@
+package com.yongli.config;
+
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EsConfig {
+
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient(){
+        return new RestHighLevelClient
+                (RestClient.builder(new HttpHost("10.20.31.205",9302,"http")));
+    }
+}
