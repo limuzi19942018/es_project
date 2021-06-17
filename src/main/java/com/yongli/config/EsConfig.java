228e4d2be92aa1b3670bc2f65ef80a7f0660e81c
liyong <1948505413@qq.com>
2021-06-17 18:47:50 +0800

    es测试类索引的相关操作

--- src/main/java/com/yongli/config/EsConfig.java
+++ src/main/java/com/yongli/config/EsConfig.java
@@ -11,9 +11,12 @@ import org.springframework.context.annotation.Configuration;
 public class EsConfig {
 
 
+
     @Bean
     public RestHighLevelClient restHighLevelClient(){
+        //http端口号是9200，也就是服务连接的端口号，此处的9301是用docker部署的映射
+        //tcp端口号是9300
         return new RestHighLevelClient
-                (RestClient.builder(new HttpHost("10.20.31.205",9302,"http")));
+                (RestClient.builder(new HttpHost("10.20.31.205",9301,"http")));
     }
 }
--- src/test/java/com/yongli/EsProjectApplicationTests.java
+++ src/test/java/com/yongli/EsProjectApplicationTests.java
@@ -1,13 +1,59 @@
 package com.yongli;
 
+import com.yongli.config.EsConfig;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EsProjectApplicationTests {
 
+
+	@Autowired
+	private RestHighLevelClient client;
+
+
+	/**
+	 * 创建一个索引
+	 * @throws Exception
+	 */
 	@Test
-	void contextLoads() {
+	void createIndex() throws Exception{
+		//创建索引请求
+		CreateIndexRequest request = new CreateIndexRequest("test_index");
+		//执行请求
+		CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
+		System.out.println(response);
+	}
+
+
+	/**
+	 * 判断es库里面的索引是否存在
+	 * @throws Exception
+	 */
+	@Test
+	void testExistIndex()throws Exception{
+		GetIndexRequest request = new GetIndexRequest("test_index");
+		boolean b = client.indices().exists(request, RequestOptions.DEFAULT);
+		System.out.println(b);
+	}
+
+	/**
+	 * 删除索引
+	 * @throws Exception
+	 */
+	@Test
+	void testDeleteIndex()throws Exception{
+		DeleteIndexRequest request = new DeleteIndexRequest("label2");
+		AcknowledgedResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
+		System.out.println(response.isAcknowledged());
 	}
 
 }
